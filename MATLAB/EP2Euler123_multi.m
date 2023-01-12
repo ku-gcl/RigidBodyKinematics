214404c1e89f59d05fbe6e3ed89f6655ecc76120
SHIO777 <shiotatsu1015@gmail.com>
2023-01-12 18:44:49 +0900

    [fix] EP2Euler123_multi

--- MATLAB/EP2Euler123_multi.m
+++ MATLAB/EP2Euler123_multi.m
@@ -16,5 +16,4 @@ for i = 1:m
     e(i, 1) = atan2(-2*(q2*q3-q0*q1),q0*q0-q1*q1-q2*q2+q3*q3);
     e(i, 2) = asin(2*(q1*q3 + q0*q2));
     e(i, 3)= atan2(-2*(q1*q2-q0*q3),q0*q0+q1*q1-q2*q2-q3*q3);
-e=e';
 end
